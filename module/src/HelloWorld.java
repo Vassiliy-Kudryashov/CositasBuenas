e7ff2409aa82285e82d634ab7e0e172b6327c8f6
Vassiliy <vassiliy.kudryashov@jetbrains.com>
2014-02-24 19:50:10 +0400

    initial

--- module/src/HelloWorld.java
+++ module/src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public HelloWorld() {
         JFrame frame = new JFrame("Hello world");
         JLabel label = new JLabel();
-        label.setFont(new Font("Serif", Font.PLAIN, 33));
+        label.setFont(new Font("Serif", Font.PLAIN, 32));
         label.setText("Hello World!");
         frame.getContentPane().add(label);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
