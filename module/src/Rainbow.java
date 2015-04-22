82e39c9c8d25fb8b0fef10eb1f20789c1f67943a
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2015-04-22 23:17:08 +0300

    Rainbow noisy images generator

--- module/src/Rainbow.java
+++ module/src/Rainbow.java
@@ -1,13 +1,15 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 /**
- * Created by user on 18.08.14.
+ * Cr eated by user on 18.08.14.
  */
 public class Rainbow {
+
     public static void main(String[] args) throws IOException {
         File base = new File("/Users/user/Pictures/colors/");
         base.mkdirs();
@@ -17,20 +19,31 @@ public class Rainbow {
             boolean b = Math.random()>.5;
             int w = b? 1440: 900;
             int h = b? 900: 1440;
-            int size = (int)(Math.sqrt(w*w + h*h)) / 2;
+            double cx = Math.random() * w;
+            double cy = Math.random() * h;
+            Point2D c = new Point.Double(cx, cy);
+            double size = c.distance(0, 0);
+            size = Math.max(size, c.distance(0, w));
+            size = Math.max(size, c.distance(h, w));
+            size = Math.max(size, c.distance(h, 0));
+//            int size = (int)(Math.sqrt(w*w + h*h)) / 2;
             BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
             Graphics graphics = image.getGraphics();
-            for (int i = 0; i < w/side; i++) {
-                for (int j = 0; j < h/side; j++) {
+            for ( int i = 0; i <= w/side; i++) {
+                for (int j = 0; j <= h /side; j++) {
                     int x = i * side;
                     int y = j * side;
-                    double distance = new Point(x, y).distance(w / 2, h / 2) / size;//0..1
-                    int rgb = Color.HSBtoRGB(((float)hue)/100, getRandom(.66, 1 - distance * .33), getRandom(.5, 1 - distance * .5));
+                     double distance = new Point(x, y).distance(cx, cy) / size;//0..1
+                    int rgb = Color.HSBtoRGB(((float)hue)/100, getRandom(1 - distance / 4, 1), getRandom(1 - distance /3, 1));
                     graphics.setColor(new Color(rgb));
                     graphics.fillRect(x, y, side, side);
                     graphics.drawRect(x, y, side, side);
                 }
             }
+
+
+
+
 //            graphics.fillRect(0, 0, w+1, h+1);
             ImageIO.write(image, "png", new File(base, ""+hue + ".png"));
         }
@@ -39,3 +52,25 @@ public class Rainbow {
         return Math.min(1, Math.max(0, (float) (min + Math.random() * (max - min))));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
