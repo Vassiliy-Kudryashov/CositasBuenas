d4dea2a308f6d35c23708073c7154291c26a5b3c
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2016-10-07 19:08:14 +0300

    just another revision

--- module/src/CodeGen.java
+++ module/src/CodeGen.java
@@ -1,9 +1,7 @@
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOError;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -12,10 +10,12 @@ import java.util.Map;
 public class CodeGen {
 
     public static final File BASE = new File("module/src");
-    private static Map<File, String> fileToMethod = new HashMap<File, String>();
+    private static  Map<File, String> fileToMethod = new HashMap<File, String>();
+
+    private int unused;
 
     public static void main(String[] args) {
-        for (int i = 0; i < 30; i++) {
+        for(int i = 0; i < 30; i++) {
             File p = new File(BASE, "p" + i);
             p.mkdir();
             for (int j = 0; j < 30; j++) {
@@ -27,13 +27,102 @@ public class CodeGen {
                     StringBuilder sb = new StringBuilder("package " + p.getName() + ";\n");
                     sb.append("public class " + name + " {\n");
                     String methodName = "m" + name;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                     sb.append("  public static void " + methodName + "() {\n");
                     for (Map.Entry<File, String> entry : fileToMethod.entrySet()) {
                         File f = entry.getKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                         String fName = f.getName().substring(0, f.getName().length() - 5);
                         sb.append("    ").append(f.getParentFile().getName() + "." + fName + "." + entry.getValue() + "();\n");
                     }
                     fileToMethod.put(file, methodName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                     sb.append("  }\n");
                     sb.append("}");
                     fos.write(sb.toString().getBytes());
@@ -52,3 +141,9 @@ public class CodeGen {
         }
     }
 }
+//       1         2         3         4         5         6         7        8         9         A         B         C12
+/*
+System
+
+
+ */
