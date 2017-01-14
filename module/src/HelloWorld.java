948b195599c039a301ca902f605249a2609c16f9
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2017-01-14 18:59:21 +0300

    IDEA-164380

--- module/src/HelloWorld.java
+++ module/src/HelloWorld.java
@@ -1,18 +1,123 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 
 public class HelloWorld {
     public HelloWorld() {
-        JFrame frame = new JFrame("Hello world");
-        JLabel label = new JLabel();
-        label.setFont(new Font("Serif", Font.PLAIN, 32));
-        label.setText("Hello World!");
+        JFrame frame = new JFrame("Hello World");
+        JLabel label = new JLabel("Click me");
+        label.setFont(new Font("Serif", Font.PLAIN, 42));
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.isAltDown()) {
+                    System.err.println("Error!");
+                } else {
+                    System.out.println(e);
+                }
+            }
+        });
         frame.getContentPane().add(label);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public static void main(String[] args) {
         new HelloWorld();
