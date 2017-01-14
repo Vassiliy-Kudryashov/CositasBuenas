6f8cfe26a15c40cb8753eb7c1db5eab73cd64986
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2017-01-14 23:36:37 +0300

    IDEA-164380

---
+++ module/src/EditorTooltipsExample.java
@@ -0,0 +1,11 @@
+public class EditorTooltipsExample {
+    private static Object o1;
+    private static  Object o2;
+    private static Object o3;
+
+    public EditorTooltipsExample() {
+        o1 = 1;
+        o2 = 2;
+        o3 = 3;
+    }
+}
