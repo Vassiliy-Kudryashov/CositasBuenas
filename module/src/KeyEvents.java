06cca59d33f67a278e32e64d59010438dbe21562
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2017-01-14 19:53:25 +0300

    Rainbow noisy images generator

--- module/src/KeyEvents.java
+++ module/src/KeyEvents.java
@@ -1,4 +1,3 @@
-
 import javax.swing.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
@@ -8,9 +7,10 @@ import java.awt.event.KeyEvent;
  */
 public class KeyEvents extends JFrame {
     public static void main(String[] args) {
+        System.out.println("KeyEvents #" + System.currentTimeMillis() % 222);
         KeyEvents  keyEvents = new KeyEvents();
         final JLabel label = new JLabel();
-        keyEvents.setContentPane(label );
+        keyEvents.setContentPane(label );//////////////////////////////////////////////////////////////////////////////////
         keyEvents.getContentPane().addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
@@ -20,7 +20,76 @@ public class KeyEvents extends JFrame {
         keyEvents.setSize(500, 400);
         keyEvents.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         keyEvents.setVisible(true);
+
+        Integer test_property = Integer.getInteger("test_property");
+//        List<String> test = new ArrayList<String>(test_property);
+//        ImageIcon icon = new ImageIcon(KeyEvents.class.getResource("../test.png"));
+        new Exception().printStackTrace();
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+/**
+ * getProperty("filterNewlines")
+ sw
+ sw
+ s
+ w
+ s
+ w
+ sw
+ s
+ w
+ */
 
