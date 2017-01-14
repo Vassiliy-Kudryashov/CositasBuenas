163b005ec37487f19c489694aacac2a0cd83a072
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2017-01-14 22:09:07 +0300

    IDEA-164380

--- module/tests/TestTestTest.java
+++ module/tests/TestTestTest.java
@@ -6,8 +6,13 @@ import java.awt.*;
  * Created by user on 05.05.14.
  */
 public class TestTestTest extends TestCase {
+
     public void testOne() throws InterruptedException {
-        Thread.sleep(15000);
+        Thread.sleep(2000);
         System.out.println(GraphicsEnvironment.isHeadless());
+    }
+
+    public void testFailure2() {
+        fail();
     }
 }
