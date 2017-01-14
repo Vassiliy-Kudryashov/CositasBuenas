8b1c65e3437f7a2bda9659ffbd365eb91c677526
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2017-01-14 22:08:27 +0300

    IDEA-164380

---
+++ anothermodule/src/AnotherClass.java
@@ -0,0 +1,11 @@
+import java.awt.event.MouseListener;
+
+/**
+ * Created by user on 26/09/15.
+ */
+public class AnotherClass {
+    //UIProperties, yes!
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("z"));
+    }
+}
