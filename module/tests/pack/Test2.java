6845be2f14b66b36b71328b6d7b63277c667945c
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2017-05-30 17:14:55 +0300

    IDEA-170239 Hyperlink click is handled despite mouse clicking outside of it

---
+++ module/tests/pack/Test2.java
@@ -0,0 +1,14 @@
+package pack;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by user on 24.10.14.
+ */
+public class Test2 extends TestCase{
+    public void testOK() {}
+
+    public void testFailure() {
+        fail();
+    }
+}
