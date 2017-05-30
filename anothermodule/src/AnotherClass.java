2e5833994fc49a805389c67effd69ee1d1c9de2c
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2017-05-30 09:44:16 +0300

    Push dialog test

--- anothermodule/src/AnotherClass.java
+++ anothermodule/src/AnotherClass.java
@@ -6,6 +6,7 @@ import java.awt.event.MouseListener;
 public class AnotherClass {
     //UIProperties, yes!!
     public static void main(String[] args) {
+
         System.out.println(System.getProperty("z"));
     }
 }
