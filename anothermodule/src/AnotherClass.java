770e544bf7f93b4cd54b0170fc579ee4ded4c5c1
Vassiliy.Kudryashov <vassiliy.kudryashov@jetbrains.com>
2019-07-24 13:22:44 +0300

    revision 2

--- anothermodule/src/AnotherClass.java
+++ anothermodule/src/AnotherClass.java
@@ -1,12 +1,70 @@
-import java.awt.event.MouseListener;
+import java.awt.*;
+import java.io.IOException;
 
 /**
  * Created by user on 26/09/15.
  */
 public class AnotherClass {
-    //UIProperties, yes!!
-    public static void main(String[] args) {
+    public int a;
+    private int b;
+    private String c;
 
-        System.out.println(System.getProperty("z"));
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    //UIProperties, yes!!sdjlksdklj
+    public static void main(String[] args) throws IOException {
+        System.out.println(System.getProperty("foo"));
+        Color[] cc = new Color[]{new Color(255, 0, 0)};
+        Color[] cc2 = new Color[]{new Color(255, 0, 0), new Color(0, 255, 0)};
+        Color[] cc3 = new Color[]{new Color(255, 0, 0), new Color(0, 255, 0, 0)};
+        Color[] cc4 = new Color[]{new Color(255, 0, 0, 0), new Color(0, 255, 0)};
+        Color[] cc5 = new Color[]{new Color(255, 0, 0), new Color(0, 255, 0), new Color(0, 0, 255)};
+        Color[] cc6 = new Color[]{new Color(0xff0000), new Color(98, 255, 65, 0), new Color(0x75A2FF), new Color(0xffac00)};
+        Color[] cc7 = new Color[]{new Color(0xff0000), new Color(0),new Color(0x62FF41), new Color(0x75A2FF),  new Color(0xffac00)};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        while (true) {
+//            String line = reader.readLine();
+//            if (line != null) {
+//                System.out.println(line);
+//            } else {
+//                break;
+//            }
+//        }
+
+
+
+
+
+
     }
 }
